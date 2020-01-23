f7df31696602e88627fd4d7859707bd29be908c9
Connor Jones <ConnorLeeJones@gmail.com>
2020-01-23 17:38:35 -0500

    adding artist follow functionality

--- src/main/java/com/connor/demo/controller/ArtistController.java
+++ src/main/java/com/connor/demo/controller/ArtistController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class ArtistController {
@@ -31,8 +33,8 @@ public class ArtistController {
     }
 
     @PostMapping("/artists")
-    public ResponseEntity<Artist> addArtist(@RequestBody Artist artist) {
-        return new ResponseEntity<>(artistService.addArtist(artist), HttpStatus.OK);
+    public ResponseEntity<Artist> addArtist(HttpServletRequest request, @RequestBody Artist artist) {
+        return new ResponseEntity<>(artistService.addArtist(request, artist), HttpStatus.OK);
     }
 
 
--- src/main/java/com/connor/demo/service/ArtistService.java
+++ src/main/java/com/connor/demo/service/ArtistService.java
@@ -4,46 +4,38 @@ import com.connor.demo.model.Artist;
 import com.connor.demo.model.User;
 import com.connor.demo.repository.ArtistRepository;
 import com.connor.demo.repository.UserRepository;
-import com.connor.demo.security.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class ArtistService {
 
     private ArtistRepository artistRepository;
     private UserRepository userRepository;
+    private UserService userService;
 
 
     @Autowired
-    public ArtistService(ArtistRepository artistRepository, UserRepository userRepository) {
+    public ArtistService(ArtistRepository artistRepository, UserRepository userRepository, UserService userService) {
         this.artistRepository = artistRepository;
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     public Iterable<Artist> findAll(){return artistRepository.findAll();}
 
 
-    public Artist addArtist(Artist artist) {
-        if (!artistRepository.existsById(artist.getId())) {
-            User user = userRepository.findUserById(1L);
-
-
-
+    public Artist addArtist(HttpServletRequest request, Artist artist) {
+            User user = userService.getUserByToken(request);
 
             artist.getUserProfile().add(user.getUserProfile());
             artistRepository.save(artist);
             user.getUserProfile().getArtists().add(artist);
             userRepository.save(user);
-//            artistRepository.save(artist);
-
 
             return artistRepository.save(artist);
-        } else {
-            throw new CustomException("Artist already exists", HttpStatus.UNPROCESSABLE_ENTITY);
-        }
     }
 
 }
